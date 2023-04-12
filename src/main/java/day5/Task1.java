1ac7d177dbeb6703e16a9c340c33fd3d7e61caa4
Svetlana Valentinovna <svet6988@yandex.ru>
2023-04-12 19:20:22 +0300

    add days 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("BMV");
+        car.setColor("Красная");
+        car.setAge(2022);
+        System.out.println(car.getModel() + " " + car.getColor() + " " + car.getAge());
+
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Ява", "синий", 1986);
+        System.out.println(motorbike.getModel() + " " + motorbike.getColor() + " " + motorbike.getAge());
 
     }
 }
