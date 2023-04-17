142684fd65ef75fe323301c1c271c22e29b1e21a
Svetlana Valentinovna <svet6988@yandex.ru>
2023-04-17 18:04:28 +0300

    add days 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,35 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+
+        String countString = "";
+
+
+        long startTime = System.currentTimeMillis();
+            for (int i = 0; i <= 20000; i++) {
+                countString += i + " ";
+        }
+
+       long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы String, в мс.: " + (stopTime-startTime));
+
+        System.out.println(countString);
+
+
+
+        StringBuilder sb = new StringBuilder("");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+
+        stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы StringBuilder, в мс.: " + (stopTime-startTime));
+
+        System.out.println(sb);
+
+
+
     }
 }
