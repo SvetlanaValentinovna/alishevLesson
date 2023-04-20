10a84361d9fc2cd98a1176b1edce0fe14e26f2ea
Svetlana Valentinovna <svet6988@yandex.ru>
2023-04-20 11:17:33 +0300

    add days 9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,12 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Иван", "25B");
+        Teacher teacher = new Teacher("Борис Николаевич", "Информатика");
+
+        student.printInfo();
+        teacher.printInfo();
+
 
     }
 }
